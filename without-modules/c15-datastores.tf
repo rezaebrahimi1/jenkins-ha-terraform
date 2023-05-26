4784faa11d44d934bbf115d5d5b33efcf771f002
Your Name <you@example.com>
2023-05-26 20:26:47 +0330

    Add template to be use in user-data for bastion host port manipulation script

--- without-modules/c15-datastores.tf
+++ without-modules/c15-datastores.tf
@@ -6,3 +6,9 @@ data "aws_ami" "ami_raw" {
     values = [var.ami_name]
   }
 }
+data "template_file" "ssh_port_modify" {
+  template = file("scripts/ssh_port_modify.ssh")
+  vars = {
+    ssh_port = var.bastion_ssh_port
+  }
+}
